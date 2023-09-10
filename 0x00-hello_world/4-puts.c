8eada979e883fc1b80959346b4491f677bbb2757
Kyle-Muloki <kylemuloki12@gmai.com>
2023-09-11 02:10:29 +0300

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- this is the main function
+ * Return: always return 0
+ */
+
+int main ()
+
+	puts("\"Programming is like building a multilingual puzzle);
+
+	return (0);
