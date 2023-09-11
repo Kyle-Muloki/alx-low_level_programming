6d47af59ac3e197b064b6b3aec510d041ac53eaa
Kyle-Muloki <kylemuloki12@gmai.com>
2023-09-11 03:01:13 +0300

    Print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is the function that introduces other functions
+ * Return: 0 when its complete
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
