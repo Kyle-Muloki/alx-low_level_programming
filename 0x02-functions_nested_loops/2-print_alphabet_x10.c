ea7b5964ef1e2d9de86aa38998e58dca21f849f8
Kyle-Muloki <kylemuloki12@gmai.com>
2023-09-14 03:08:16 +0300

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet at lowercase 10x
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int b;
+	char d;
+
+	for (b = 0 ; b < 10 ; b++)
+	{
+		for (d = 'a' ; d <= 'z' ; d++)
+			_putchar(d);
+	}
+	_putchar('\n');
+}
